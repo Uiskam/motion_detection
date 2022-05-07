7181620ca3a10304dafa067cb117e3e806494cc5
Loloxon <nikodemko@interia.pl>
2022-05-07 14:29:43 +0200

    changes

--- nk/main.py
+++ nk/main.py
@@ -27,20 +27,24 @@ ret, frame2 = cap.read()
 print(frame1.shape)
 while cap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(diff, (25, 25), 0)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
+        # print(x,y,w,h)
+        # for contour2 in contours:
+        #     (x2, y2, w2, h2) = cv2.boundingRect(contour2)
+        #     if not (x2 <= x and x2 + w2 >= x + w and y2 <= y and y2 + h2 >= y + h):
 
-        if cv2.contourArea(contour) < 900:
+        if cv2.contourArea(contour) < 10:
             continue
-        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (0, 0, 255), 3)
+                    1, (0, 255, 0), 3)
     # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
 
     image = cv2.resize(frame1, (1280, 720))
